e1c07716cc10d62d87f486a43fa777007c77e558
kangtaehee <rkdxogml5768@naver.com>
2018-08-06 20:51:47 +0900

    admin product api

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
 //node 가져오기
 var express = require('express');
 
 //MongoDB 접속
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 var autoIncrement = require('mongoose-auto-increment');
 
 var db = mongoose.connection;
@@ -17,8 +22,19 @@ autoIncrement.initialize(connect);
 //route 연결
 var admin = require('./routes/admin');
 
+//port설정
 var app = express();
 var port = 4000;
+
+// 확장자가 ejs 로 끈나는 뷰 엔진을 추가한다.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// 미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/', function (req,res) {
     res.send('first app');
